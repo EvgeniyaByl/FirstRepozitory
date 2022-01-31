876932a861efc5c48bc15456a38d9b845afe52ea
Evgeniya Byl <1468848@gmail.com>
2022-01-31 15:55:54 +0300

    Add files

---
+++ main.js
@@ -0,0 +1,21 @@
+//ts-check;
+"use strict";
+let login = prompt('Login111?', '');
+
+if (login == ('' || null)) {
+    alert('Отмена');
+}
+else if (login == 'Админ') {
+    let pass = prompt('Пароль?', '');
+    if (pass == 'Я главный') {
+        alert('Здравствуйте!');
+    }
+    else if (pass == ('' || null)) {
+        alert('Отменено');
+    } else {
+        alert('Неверный пароль');
+    }
+}
+else {
+    alert('Я вас не знаю');
+}
